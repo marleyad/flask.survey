5cb0d21a6076fb2949184a657bf3b73b92fb0d74
Adam Marley <141704661+marleyad@users.noreply.github.com>
2024-09-26 13:43:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-
 @app.route("/")
 def show_survey_start():
     """Select a survey."""
 
     return render_template("survey_start.html", survey=survey)
-
 
 @app.route("/begin", methods=["POST"])
 def start_survey():
